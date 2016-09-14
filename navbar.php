e532f3b5753d9c905f5ca1500294e099afcf29f0
darwyncook <cook@alfred.edu>
2016-09-14 14:03:44 -0400

    Create navbar.php

---
+++ navbar.php
@@ -0,0 +1,8 @@
+<h1> Alfred University </h1>
+ <div class="nav">
+        <ul>
+            <li><a href="/integrationbee/">Home</a></li>
+            <li><a href="/integrationbee/registration/">Registration</a></li>
+            <li><a href="/integrationbee/history/">Past Winners</a></li>
+        </ul>
+ </div>
